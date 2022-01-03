bc3d9eb145a263e8bbb12670a33680a38a89a0ef
umut9191 <umutsurmeli91@gmail.com>
2022-01-04 01:12:53 +0300

    App just to show how to use dark, light modes in iPhone. It is complated

--- DarkModeApp/ViewController.swift
+++ DarkModeApp/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cahngeButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //this is for independent change current controller screen mode doesnt care about what mode using in phone currently
+        //overrideUserInterfaceStyle = .dark
+        
+        // if we want all app screens be as what we want mode than we go info.plist file
+        // user ınterface style --> Dark,Light
+        //but xcode 12 UIUserInterfaceStyle instade of user ınterface style
+     
     }
-
+ 
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        let userInterfaceStyle = traitCollection.userInterfaceStyle
+        // if user phone in dark mode;
+        if userInterfaceStyle == .dark {
+            cahngeButton.tintColor = UIColor.white
+        }
+        if userInterfaceStyle == .light {
+            cahngeButton.tintColor = UIColor.blue
+        }
+    }
 
 }
 
